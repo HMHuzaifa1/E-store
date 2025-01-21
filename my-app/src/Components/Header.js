397d0a8c15f374f17469ed8af0fd6a156ccd2330
HMHuzaifa1 <hafizhuzaifa734@gmail.com>
2025-01-22 02:37:56 +0500

    commit

--- my-app/src/Components/Header.js
+++ my-app/src/Components/Header.js
@@ -7,7 +7,7 @@ import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import Offcanvas from "react-bootstrap/Offcanvas";
 import { Link } from 'react-router-dom';
-import { FaOpencart } from "react-icons/fa6";
+// import { FaOpencart } from "react-icons/fa6";
 
 
 
